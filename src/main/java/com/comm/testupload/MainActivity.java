b6105b8265e9f795662fcbca1e6ddcf56205ecfe
yangj <445208195@qq.com>
2021-09-18 11:39:45 +0800

    test upload1

--- src/main/java/com/comm/testupload/MainActivity.java
+++ src/main/java/com/comm/testupload/MainActivity.java
@@ -3,14 +3,15 @@ package com.comm.testupload;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+ Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+  button = findViewById(R.id.accelerate);
 
     }
 }
